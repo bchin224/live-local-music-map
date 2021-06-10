4a93a86a29e8429fc72d77a22a9faba9b7c1efb0
bchin224 <bchin224@gmail.com>
2021-06-10 18:10:57 -0400

    google maps integration, adding marker state

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Getting Started with Create React App
+# 'Live Local Music Map'
+A map for finding and marking the local spots where you can find live music.
+Whether it be a bar, park, club or dedicated venue, mark it and share the music!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Links
+
+- [Create React App](https://github.com/facebook/create-react-app)
+- [React Google Maps API](https://www.npmjs.com/package/@react-google-maps/api)
 
 ## More info coming soon
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+// import { formatRelative } from 'date-fns';
+
+// set libraries to it's own variable to prevent rerendering by react
+const libraries = ['places']
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+}
+const center = {
+  lat: 40.7128,
+  lng: -74.0060,
+}
+const options = {
+  // add map styles if you'd like to edit the way the map looks
+  // this will remove the default pins, zoom control, on the map
+  disableDefaultUI: true,
+  streetViewControl: true,
+  zoomControl: true
 }
 
-export default App;
+export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+  // set state
+  const [markers, setMarkers] = React.useState([])
+
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return 'Loading Maps';
+
+  return <div>
+    <h1>Live Local Music Map
+      <span role='img' aria-label='music'> 🎶</span>
+    </h1>
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={12}
+      center={center}
+      options={options}
+      onClick={(event) => {
+        setMarkers(current => [
+          ...current,
+          {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date(),
+          },
+        ])
+      }
+      }
+    ></GoogleMap>
+  </div>
+}
